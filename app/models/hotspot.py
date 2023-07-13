bb743916c558942b84b132de263b7c2c4ef62600
Jason Breedlove <jbreedlove@jasonbreedlove.dev>
2023-07-13 13:37:29 -0700

    create db

---
+++ app/models/hotspot.py
@@ -0,0 +1,11 @@
+from app import db
+
+
+class Hotspot(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+    wifi_quality = db.Column(db.Integer)
+    has_power = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return '<Hotspot {}>'.format(self.name)
